faeeae7abad41e7b003bdf4108840a899494540b
Onur Deniz Yılmaz <onurdennizylmz@hotmail.com>
2023-02-24 19:36:39 +0300

    push

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
+
+const TodoForm = ({setTodos,todos}) => {
+    const [todoValue, setTodoValue] = useState('')
+    
+    const handleSubmit = (e) => {
+      if(todoValue === ''){
+        toast.error('Please Enter Your Todo')
+      }else if (todos.includes(todoValue)){
+        toast.error('Todo Already Exists')
+      }else if (todos.length < 5){
+        toast.error('Minimum characters required is 5')
+      }else{
+        const newTodo = todoValue.trim()
+      
+
+
+        setTodos([...todos, newTodo])
+        toast.success('Todo Added Successfully')
+      }
+    }
+        
+
+  return (
+    <form action="" id='todo-form'>
+      <input
+         type="text" 
+         id='form-input ' 
+         placeholder='Please Enter Your Todo ' 
+         onChange={(e) => setTodoValue(e.target.value)}
+         value={todoValue}
+        />
+        <button onClick={(e)=>{
+            e.preventDefault()
+            handleSubmit()
+            setTodoValue('')
+        
+        }}
+      >
+        <i class="fa-solid fa-plus"></i>
+      </button>
+    </form>
+    
+  )
+    
+}
+
+export default TodoForm 
+
+    
